3f88a3115c3109b955e4e7ca128f070000948793
Abe Rudy <aberudy@gmail.com>
2026-02-05 11:48:06 -0500

    small modification to formatting

--- src/FileCabinet/SuiteScripts/Myers-Holum/CustomHtmlFields/MHI_ECL_CustomHtmlEntry_UE.js
+++ src/FileCabinet/SuiteScripts/Myers-Holum/CustomHtmlFields/MHI_ECL_CustomHtmlEntry_UE.js
@@ -2,7 +2,11 @@
  * @NApiVersion 2.1
  * @NScriptType UserEventScript
  */
-define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RUNTIME) => {
+define(["N/search", "N/ui/serverWidget", "N/runtime"], (
+  SEARCH,
+  serverWidget,
+  RUNTIME,
+) => {
   function beforeLoad(context) {
     const { type } = context;
     if (
@@ -16,23 +20,23 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
     const form = context.form;
     const recType = rec.type;
 
-    form.clientScriptModulePath = './MHI_ECL_CustomHtmlEntry_CS.js';
+    form.clientScriptModulePath = "./MHI_ECL_CustomHtmlEntry_CS.js";
 
     const { results, groupedRec } = getFieldConfigs(recType);
-    log.audit('Running HTML Config Search', results);
+    log.audit("Running HTML Config Search", results);
 
     const indexes = results.map((ent, ind) => `.custom-radio-label-${ind}`);
 
-    let htmlStr = '';
+    let htmlStr = "";
     if (results.length > 0) {
       const form = context.form;
 
-      const tabId = 'custpage_mhi_ecl_html_tab';
+      const tabId = "custpage_mhi_ecl_html_tab";
 
       // Add custom tab
       const customtab = form.addTab({
         id: tabId,
-        label: 'MHI | KF Custom Fields'
+        label: "MHI | KF Custom Fields",
       });
       // form.insertTab({
       //   tab: customtab,
@@ -46,13 +50,13 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
       // });
 
       form.addField({
-        id: 'custpage_mhi_ecl_html_field',
+        id: "custpage_mhi_ecl_html_field",
         type: serverWidget.FieldType.INLINEHTML,
-        label: 'Custom HTML',
-        container: tabId
+        label: "Custom HTML",
+        container: tabId,
       });
 
-      log.audit('indexes', indexes);
+      log.audit("indexes", indexes);
       htmlStr += `
         <style>
           .custom-radio-group {
@@ -67,10 +71,10 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
             font-size: 14px;
             padding: 4px 8px;
             border-radius: 6px;
-            transition: background 0.2s; }`
+            transition: background 0.2s; }`,
         )}
 
-          ${indexes.map((res, ind) => `${res} input[type="radio"] { display: none; }`).join('\n')}
+          ${indexes.map((res, ind) => `${res} input[type="radio"] { display: none; }`).join("\n")}
             
           .custom-radio-icon {
             width: 20px;
@@ -84,12 +88,15 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
           ${indexes
             .map(
               (res, ind) =>
-                `${res} input[type="radio"]:checked + .custom-radio-icon {  filter: drop-shadow(0 0 2px #0070d2) ; }`
+                `${res} input[type="radio"]:checked + .custom-radio-icon {  filter: drop-shadow(0 0 2px #0070d2) ; }`,
             )
-            .join('\n')}
+            .join("\n")}
           ${indexes
-            .map((res, ind) => `${res} iinput[type="radio"]:checked ~ span {  color: #0070d2; }`)
-            .join('\n')}
+            .map(
+              (res, ind) =>
+                `${res} iinput[type="radio"]:checked ~ span {  color: #0070d2; }`,
+            )
+            .join("\n")}
             
           }
             ${indexes.map((res, ind) => `${res}:hover { background: #f0f4fa; }`)}
@@ -99,10 +106,10 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
       groups.forEach((group) => {
         log.audit({
           title: group,
-          details: { g: groupedRec[group] }
+          details: { g: groupedRec[group] },
         });
         const groupData = groupedRec[group];
-        htmlStr += `<div style="flex:2; margin:10px; border:1px solid #eee; border-radius:8px; padding:16px; ">`;
+        htmlStr += `<div style="width:100%; flex:1; margin:10px; border:1px solid #eee; border-radius:8px; padding:16px; ">`;
 
         htmlStr += `<div style="margin-bottom:4px;">`;
         if (groupData.title) {
@@ -121,23 +128,24 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
           htmlStr += `<p style="font-size:13px; color:#666; margin-bottom:12px;">${groupData.desc}</p>`;
         }
         htmlStr += `</div>`;
-        log.audit('groupData.fields', groupData.fields);
+        log.audit("groupData.fields", groupData.fields);
 
         groupData.fields.forEach((result, ind) => {
           htmlStr += `<div class="custom-radio-group">`;
-          const text = result.getValue('custrecord_mhi_ecl_cust_html_text');
-          const radio = result.getValue('custrecord_mhi_ecl_cust_html_radio');
-          const label = result.getValue('custrecord_mhi_ecl_cust_html_label');
-          const url = result.getValue('custrecord_mhi_ecl_cust_html_target');
+          const text = result.getValue("custrecord_mhi_ecl_cust_html_text");
+          const radio = result.getValue("custrecord_mhi_ecl_cust_html_radio");
+          const label = result.getValue("custrecord_mhi_ecl_cust_html_label");
+          const url = result.getValue("custrecord_mhi_ecl_cust_html_target");
           const textVal = rec.getValue(text);
           const radioVal = rec.getValue(radio);
-          log.audit('Field Values', {
+          log.audit("Field Values", {
             text,
             textVal,
             radio,
-            radioVal
+            radioVal,
           });
-          const htmlText = result.getValue('custrecord_mhi_ecl_cust_html_text') || '';
+          const htmlText =
+            result.getValue("custrecord_mhi_ecl_cust_html_text") || "";
 
           // htmlStr += `<div style="margin-bottom:10px;">`;
 
@@ -152,23 +160,23 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
           }
           htmlStr += `<br/>`;
           htmlStr += `<input ${
-            type === context.UserEventType.VIEW ? 'disabled' : ''
-          } value="${textVal}" type="text" id="custpage_${text.split('_').slice(1).join('_')}" 
+            type === context.UserEventType.VIEW ? "disabled" : ""
+          } value="${textVal}" type="text" id="custpage_${text.split("_").slice(1).join("_")}" 
             style="width:30%; border-radius:6px; margin-right:10px; padding:4px 8px; border:1px solid #ccc;" 
             onchange="(function(e) { debugger; console.log('changed the text input'); require(['N/currentRecord'], function(CR) { const curr = CR.get(); curr.setValue('${text}',document.querySelector('#custpage_${text
-            .split('_')
-            .slice(1)
-            .join('_')}').value) }); })()"/>`;
+              .split("_")
+              .slice(1)
+              .join("_")}').value) }); })()"/>`;
           if (radio)
             htmlStr += `<label class="custom-radio-label-${ind}">
-                <input type="radio" ${radioVal === '1' ? 'checked' : ''} ${
-              type === context.UserEventType.VIEW ? 'disabled' : ''
-            } name="cust-radio-${ind}" id="custpage_${radio
-              .split('_')
-              .slice(1)
-              .join(
-                '_'
-              )}_flag" onchange="(function(e) { console.log('Setting Red Flag input'); require(['N/currentRecord'], function(CR) { const curr = CR.get(); curr.setValue('${radio}','1') }); })()" />
+                <input type="radio" ${radioVal === "1" ? "checked" : ""} ${
+                  type === context.UserEventType.VIEW ? "disabled" : ""
+                } name="cust-radio-${ind}" id="custpage_${radio
+                  .split("_")
+                  .slice(1)
+                  .join(
+                    "_",
+                  )}_flag" onchange="(function(e) { console.log('Setting Red Flag input'); require(['N/currentRecord'], function(CR) { const curr = CR.get(); curr.setValue('${radio}','1') }); })()" />
                 <span class="custom-radio-icon">
             <!-- Red flag SVG icon -->
             <svg width="18" height="18" viewBox="0 0 20 20" fill="none">
@@ -178,14 +186,14 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
                 </span>
               </label>
               <label class="custom-radio-label-${ind}">
-                <input type="radio" ${radioVal === '2' ? 'checked' : ''} ${
-              type === context.UserEventType.VIEW ? 'disabled' : ''
-            } name="cust-radio-${ind}" id="custpage_${radio
-              .split('_')
-              .slice(1)
-              .join(
-                '_'
-              )}_strength" onchange="(function(e) { console.log('Setting Strength input'); require(['N/currentRecord'], function(CR) { const curr = CR.get(); curr.setValue('${radio}','2') }); })()" />
+                <input type="radio" ${radioVal === "2" ? "checked" : ""} ${
+                  type === context.UserEventType.VIEW ? "disabled" : ""
+                } name="cust-radio-${ind}" id="custpage_${radio
+                  .split("_")
+                  .slice(1)
+                  .join(
+                    "_",
+                  )}_strength" onchange="(function(e) { console.log('Setting Strength input'); require(['N/currentRecord'], function(CR) { const curr = CR.get(); curr.setValue('${radio}','2') }); })()" />
                 <span class="custom-radio-icon">
             <!-- Strength (star) SVG icon -->
             <svg width="18" height="18" viewBox="0 0 20 20" fill="none">
@@ -199,63 +207,66 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
         htmlStr += `</div>`;
       });
 
-      htmlStr += '</div>';
+      htmlStr += "</div>";
     }
-    form.getField({ id: 'custpage_mhi_ecl_html_field' }).defaultValue = htmlStr;
+    form.getField({ id: "custpage_mhi_ecl_html_field" }).defaultValue = htmlStr;
   }
 
   const getFieldConfigs = (recType) => {
-    const recTypeId = RUNTIME.getCurrentScript().getParameter('custscript_mhi_ecl_crt_rec_type');
-    log.audit('recTypeId Param', recTypeId);
+    const recTypeId = RUNTIME.getCurrentScript().getParameter(
+      "custscript_mhi_ecl_crt_rec_type",
+    );
+    log.audit("recTypeId Param", recTypeId);
     const results = [];
     const groupedRec = {};
     const htmlConfigSearch = SEARCH.create({
-      type: 'customrecord_mhi_ecl_cust_html_config',
-      filters: [['custrecord_mhi_ecl_cust_html_type', 'anyof', recTypeId]],
+      type: "customrecord_mhi_ecl_cust_html_config",
+      filters: [["custrecord_mhi_ecl_cust_html_type", "anyof", recTypeId]],
       columns: [
-        SEARCH.createColumn({ name: 'internalid' }),
-        SEARCH.createColumn({ name: 'custrecord_mhi_ecl_cust_html_text' }),
-        SEARCH.createColumn({ name: 'custrecord_mhi_ecl_cust_html_radio' }),
-        SEARCH.createColumn({ name: 'custrecord_mhi_ecl_cust_html_target' }),
-        SEARCH.createColumn({ name: 'custrecord_mhi_ecl_cust_html_label' }),
-        SEARCH.createColumn({ name: 'custrecord_mhi_ecl_cust_html_group' }),
+        SEARCH.createColumn({ name: "internalid" }),
+        SEARCH.createColumn({ name: "custrecord_mhi_ecl_cust_html_text" }),
+        SEARCH.createColumn({ name: "custrecord_mhi_ecl_cust_html_radio" }),
+        SEARCH.createColumn({ name: "custrecord_mhi_ecl_cust_html_target" }),
+        SEARCH.createColumn({ name: "custrecord_mhi_ecl_cust_html_label" }),
+        SEARCH.createColumn({ name: "custrecord_mhi_ecl_cust_html_group" }),
         SEARCH.createColumn({
-          name: 'name',
-          join: 'custrecord_mhi_ecl_cust_html_group'
+          name: "name",
+          join: "custrecord_mhi_ecl_cust_html_group",
         }),
         SEARCH.createColumn({
-          name: 'custrecord_mhi_ecl_cust_html_grp_title',
-          join: 'custrecord_mhi_ecl_cust_html_group'
+          name: "custrecord_mhi_ecl_cust_html_grp_title",
+          join: "custrecord_mhi_ecl_cust_html_group",
         }),
         SEARCH.createColumn({
-          name: 'custrecord_mhi_ecl_cust_html_grp_desc',
-          join: 'custrecord_mhi_ecl_cust_html_group'
+          name: "custrecord_mhi_ecl_cust_html_grp_desc",
+          join: "custrecord_mhi_ecl_cust_html_group",
         }),
         SEARCH.createColumn({
-          name: 'custrecord_mhi_ecl_cust_html_grp_url',
-          join: 'custrecord_mhi_ecl_cust_html_group'
-        })
-      ]
+          name: "custrecord_mhi_ecl_cust_html_grp_url",
+          join: "custrecord_mhi_ecl_cust_html_group",
+        }),
+      ],
     });
 
     htmlConfigSearch.run().each((result) => {
-      const group = result.getText('custrecord_mhi_ecl_cust_html_group') || 'MISC';
+      const group =
+        result.getText("custrecord_mhi_ecl_cust_html_group") || "MISC";
       if (group) {
         if (!groupedRec[group]) {
           groupedRec[group] = {
             title: result.getValue({
-              name: 'custrecord_mhi_ecl_cust_html_grp_title',
-              join: 'custrecord_mhi_ecl_cust_html_group'
+              name: "custrecord_mhi_ecl_cust_html_grp_title",
+              join: "custrecord_mhi_ecl_cust_html_group",
             }),
             desc: result.getValue({
-              name: 'custrecord_mhi_ecl_cust_html_grp_desc',
-              join: 'custrecord_mhi_ecl_cust_html_group'
+              name: "custrecord_mhi_ecl_cust_html_grp_desc",
+              join: "custrecord_mhi_ecl_cust_html_group",
             }),
             url: result.getValue({
-              name: 'custrecord_mhi_ecl_cust_html_grp_url',
-              join: 'custrecord_mhi_ecl_cust_html_group'
+              name: "custrecord_mhi_ecl_cust_html_grp_url",
+              join: "custrecord_mhi_ecl_cust_html_group",
             }),
-            fields: []
+            fields: [],
           };
         }
         groupedRec[group].fields.push(result);
@@ -268,42 +279,42 @@ define(['N/search', 'N/ui/serverWidget', 'N/runtime'], (SEARCH, serverWidget, RU
 
   function getCustomRecordTypeValue2(name) {
     //leverage NetSuite's URL generator to get the record type
-    return getURLParameterByName('rectype', nlapiResolveURL('RECORD', name));
+    return getURLParameterByName("rectype", nlapiResolveURL("RECORD", name));
 
     //url parser helper function
     function getURLParameterByName(name, url) {
-      log.audit('getURLParameterByName', { name, url });
-      name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-      var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+      log.audit("getURLParameterByName", { name, url });
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(url);
-      return results === null ? '' : results[1].replace(/\+/g, ' ');
+      return results === null ? "" : results[1].replace(/\+/g, " ");
     }
   }
 
   function getCustomRecordTypeValue(name) {
-    let url = require(['N/url']);
-    url = require('N/url');
+    let url = require(["N/url"]);
+    url = require("N/url");
     //leverage NetSuite's URL generator to get the record type
     return getURLParameterByName(
-      'rectype',
+      "rectype",
       url.resolveRecord({
         isEditMode: false,
-        recordType: name
+        recordType: name,
         // recordId: 1
-      })
+      }),
     );
 
     //url parser helper function
     function getURLParameterByName(name, url) {
-      log.audit('getURLParameterByName', { name, url });
-      name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-      var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+      log.audit("getURLParameterByName", { name, url });
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(url);
-      return results === null ? '' : results[1].replace(/\+/g, ' ');
+      return results === null ? "" : results[1].replace(/\+/g, " ");
     }
   }
 
   return {
-    beforeLoad
+    beforeLoad,
   };
 });
